31a1082bfce4ea95045cbc2cdd91f8a6068035e0
ahmed.saied <ahmed.saied@expertapps.com.sa>
2025-09-29 11:46:44 +0300

    feat(live data) : make the data in details screen live data

--- app/src/main/java/com/example/test_trip_logic/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/test_trip_logic/ui/fragments/DetailsFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.test_trip_logic.R
+import com.example.test_trip_logic.data.repository.TripRepository
+import com.example.test_trip_logic.data.view_model.TripViewModel
+import com.example.test_trip_logic.data.view_model.TripViewModelFactory
 import com.example.test_trip_logic.databinding.FragmentDetailsBinding
+import kotlin.getValue
 
 
 class DetailsFragment : Fragment() {
@@ -17,6 +22,9 @@ class DetailsFragment : Fragment() {
     private val binding get() = _binding!!
 
     val args: DetailsFragmentArgs by navArgs()
+    private val tripViewModel: TripViewModel by activityViewModels {
+        TripViewModelFactory(TripRepository(requireContext()))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,11 +32,12 @@ class DetailsFragment : Fragment() {
     ): View {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
 
+        val trip = tripViewModel.getTripById(args.trip.id)
+            binding.tvTripDate.text = trip?.time ?:"time"
+            binding.tvTripLat.text = trip?.lat.toString()
+            binding.tvTripName.text = trip?.name
+            binding.tvTripLng.text = trip?.lng.toString()
 
-        binding.tvTripDate.text = args.trip.time
-        binding.tvTripLat.text = args.trip.lat.toString()
-        binding.tvTripName.text = args.trip.name
-        binding.tvTripLng.text = args.trip.lng.toString()
 
         binding.fabMap.setOnClickListener {
             val action = DetailsFragmentDirections.actionDetailsFragmentToMapsFragment(
